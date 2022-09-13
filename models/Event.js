43a1805a53ab30b1e7bb0a9f6ca5a2279b01e61c
ellieeadington <ellieeadington5@gmail.com>
2022-09-13 15:49:44 +0100

    updated index.js api's to accomodate the M2M relationships between Event & Artist models.

--- models/Event.js
+++ models/Event.js
@@ -8,8 +8,8 @@ const eventSchema = mongoose.Schema({
     city: String,
     postcode: String,
     date: Date,
+    monthYear: String,
     description: String,
-
     artist: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Artist'
@@ -22,8 +22,15 @@ const eventSchema = mongoose.Schema({
     eventPhoto: String
 
 },{timestamps: true})
+
+
  
-    
+// I want to be able to, for each event, show the artists who are playing and their associated genres, 
+// meaning I need to reference the artist id's in the events collection, with the object ids in the 
+// artists collection. I also want to be able to filter the events that are displayed by the artists & their genres, 
+// referenced in the events collection. In order to do this, do I need to join the two collections using 
+// $lookup and $aggregate the data, which I will use to write the apis in my index.js controller? If so, do you have any
+// documentation on how I should go about this?
    
     
 
