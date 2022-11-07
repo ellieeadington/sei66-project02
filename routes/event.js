45c26d506faf5c9884ebd4438cab70c0bd20e850
ellieeadington <63414517+ellieeadington@users.noreply.github.com>
2022-11-07 17:04:22 +0000

    final css

--- routes/event.js
+++ routes/event.js
@@ -9,17 +9,10 @@ const eventCtrl = require("../controllers/events");
 const upload = require("../helpers/fileupload");
 
 router.get("/event/add", eventCtrl.event_create_get);
-router.post(
-  "/event/add",
-  upload.single("eventPhoto"),
-  eventCtrl.event_create_post
-);
+router.post("/event/add",upload.single("eventPhoto"),eventCtrl.event_create_post);
 router.get("/event/detail", eventCtrl.event_detail_get);
 router.get("/event/delete", eventCtrl.event_delete_get);
 router.get("/event/edit", eventCtrl.event_edit_get);
-router.post(
-  "/event/update",
-  upload.single("eventPhoto"),
-  eventCtrl.event_update_post
-);
+router.post("/event/update",upload.single("eventPhoto"),eventCtrl.event_update_post);
+
 module.exports = router;
